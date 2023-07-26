2b9c0412f1c091b7d019bc16ec4c37c3fd6592d2
pazuzu71 <genchez.tula@gmail.com>
2023-07-26 21:01:30 +0300

    requirements.txt

--- grab7_selen.py
+++ grab7_selen.py
@@ -3,17 +3,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EX
 from time import sleep
-
-
+from config import LOGIN, PASSWORD
 
 with webdriver.Chrome() as browser:
     browser.get('https://connect.garmin.com/')
-    # browser.find_element().send_keys()
-    WebDriverWait(browser, 5).until(EX.element_to_be_clickable((By.ID, "truste-consent-button"))).click()
-    WebDriverWait(browser, 5).until(EX.element_to_be_clickable((By.XPATH, '//*[@id="gatsby-focus-wrapper"]/div/header'
-                                                                          '/nav/ul/li[4]/a/button'))).click()
-    el = WebDriverWait(browser, 5).until(EX.presence_of_element_located((By.ID, 'email'))).send_keys('genchez@rambler.ru')
-    # el.send_keys('genchez@rambler.ru')
-    # browser.find_element(By.ID, 'email').send_keys('genchez@rambler.ru')
+    WDW = WebDriverWait(browser, 5)
+    WDW.until(EX.element_to_be_clickable((By.ID, "truste-consent-button"))).click()
+    WDW.until(EX.element_to_be_clickable((By.XPATH, '//*[@id="gatsby-focus-wrapper"]/div/header'
+                                                    '/nav/ul/li[4]/a/button'))).click()
+    WDW.until(EX.presence_of_element_located((By.ID, 'email'))).send_keys(LOGIN)
+    WDW.until(EX.presence_of_element_located((By.ID, 'password'))).send_keys(PASSWORD)
+    WDW.until(EX.element_to_be_clickable((By.XPATH, '//*[@id="portal"]/div[2]/div/div/div/div/form/section['
+                                                    '2]/g-button/button'))).click()
 
-    sleep(3)
+    sleep(10)
